96aeeb9d61539f6a9f1d10a04c93692dac8a8306
ぺんち <namekotoyuki100@gmail.com>
2025-07-06 16:15:50 +0900

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,14 @@
 from flask import Flask
 from threading import Thread
-import os # <--- この行を追加してください
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello! I am alive!"
+    return "Bot is running!"
 
 def run():
-    # os.environ.get を使用して安全に PORT を取得します。
-    # PORT が設定されていない場合は、デフォルトで 8080 を使用します。
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
     t = Thread(target=run)
